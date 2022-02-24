2aa531722379644fffb084d6f8667009ab4373ce
Evan Moore <even.gentry.moore@gmail.com>
2022-02-24 12:22:28 -0700

    Trying Rollbar with Node

--- server/index.js
+++ server/index.js
@@ -9,4 +9,15 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 4545
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: '2df36aea9d6c476f8b96334bacb661ec',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.listen(port , () => console.log(`Running on ${port}, for the glory of the Klingon Empire`))
