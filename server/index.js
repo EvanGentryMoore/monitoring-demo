7e4fa287bf116fbaebf2211cf37509ba3d78292b
Evan Moore <even.gentry.moore@gmail.com>
2022-02-24 12:37:00 -0700

    Added students endpoint

--- server/index.js
+++ server/index.js
@@ -6,8 +6,22 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFiles(path.join(__dirname, '../public/index.html'))
 })
+let students = []
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student was added successfully', {author: 'Scott', type:'manual', student: name})
+
+    res.status(200).send(students)
+})
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4545
+
 
 // include and initialize the rollbar library with your access token
 const Rollbar = require("rollbar");
