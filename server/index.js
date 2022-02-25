d5858737329a07c78aaf55de63916b1c83a22973
Evan Moore <even.gentry.moore@gmail.com>
2022-02-24 20:11:53 -0700

    Trying to get server/index.js to play nice with heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require('path')
 
-const app = express()
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: '2df36aea9d6c476f8b96334bacb661ec',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
-app.use(express.json())
+const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
@@ -26,12 +31,6 @@ const port = process.env.PORT || 4545
 
 
 // include and initialize the rollbar library with your access token
-const Rollbar = require("rollbar");
-const rollbar = new Rollbar({
-  accessToken: '2df36aea9d6c476f8b96334bacb661ec',
-  captureUncaught: true,
-  captureUnhandledRejections: true
-});
 
 // record a generic message and send it to Rollbar
 rollbar.log("Hello world!");
